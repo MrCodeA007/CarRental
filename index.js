f9998c240bfd3f3082f5cb426a8aa602cc16fb91
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-04 13:30:13 -0400

    car rental not yet complete-needs work

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 "use strict"
 //--------------------------------------------------------------------------
 function onBtnClickedfunction () {
-  const numberOfDaysEl = document.getElementById(`numbersOfDays`);
-  const answer = basicCarRental * numberOfDaysEl.value;
-  carRentalEl.innerHTML = answer;
+ 
+  const carRentalCost = basicCarRental * numberOfDaysEl.value;
+  carRentalEl.innerHTML = carRentalCost.toFixed(2);
   //console.log(`click`)
+  //------------------------------------------------------------------------
+  let extraDailyCost = 0;
+if (document.getElementById('tollTag').checked) {
+  extraDailyCost += 3.95;
 }
+if (document.getElementById('GPS').checked) {
+  extraDailyCost += 2.95;
+}
+if (document.getElementById('roadAssist').checked) {
+  extraDailyCost += 2.95;
+} 
+
+const totaloptionsCost = extraDailyCost * numberOfDaysEl.value;
+//console.log(`totaloptionsCost`,totaloptionsCost)
+const optionsEl = document.getElementById(`options`);
+optionsEl.innerHTML = totaloptionsCost.toFixed(2);
+//-----------------------------------------------------------------
+let under25surcharge = 0;
+if (document.getElementById('25Yes').checked) {
+  under25surcharge = carRentalEl.innerHTML * 0.3;
+}
+const under25surchargeEl = document.getElementById(`25surCharge`)
+under25surchargeEl.innerHTML = under25surcharge.toFixed(2);
+
+const totalDueEl = document.getElementById(`totalDue`)
+totalDueEl.innerHTML = under25surchargeEl.innerHTML + optionsEl.innerHTML+ carRentalEl.innerHTML;
+
+}
+
+const numberOfDaysEl = document.getElementById(`numbersOfDays`);
 const basicCarRental = 29.99;
 const  carRentalEl = document.getElementById(`carRental`);
-//const carRentalPEl = document.getElementById(`carRentalP`)
-const totalCostEl = document.getElementById(`totalCost`);
+const totalCostEl = document.getElementById(`estimatetotalCost`);
 totalCostEl.onclick = onBtnClickedfunction
 //---------------------------------------------------------------------------
-function onTollCheckedFunction (){
+
+
+
+
+
+/*function onTollCheckedFunction (){
     console.log(`checked`)
 }
 
@@ -38,4 +71,4 @@ function on25Function () {
 }
 
 const radioNoEl = document.getElementById(`25No`)
-radioNoEl.onclick = on25Function
+radioNoEl.onclick = on25Function*/
